aff3bca4659be045a7eee1bac49482cc61c86dbb
Andrew Lu <andrewclu98@gmail.com>
2020-05-06 13:23:06 -0700

    remove unnecessary navbar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import NavigationBar from "../common/NavigationBar";
 import Routes from "../common/Routes";
 
 class App extends React.Component {
   render() {
     return (
         <div className="App">
-          <NavigationBar />
           <Routes />
         </div>
     )
